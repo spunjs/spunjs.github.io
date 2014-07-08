d305c1540c217bc4a3f3f4a0a139f36930c974a2
jsdevel <js.developer.undefined@gmail.com>
2014-07-08 11:12:44 -0700

    Adding a deploy script.

---
+++ deploy.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[@]}))
+
+cd $DIR
+
+hasChanges="`git status --porcelain`"
+
+if [ -n "$hasChanges" ];then
+  echo "Changes were found!  All changes must be checked in before deploying."
+  exit 1
+fi
+
+gulp
+
+git checkout master
+mv dist/* .
+rm -rf dist/
+git add --all
+git commit -a -m 'Deploying'
